6f02fd0d4cadc754d233416b197b25132d31078c
Varsha <varsha.savukar@gmail.com>
2024-05-07 12:15:44 +0530

    changed files

---
+++ ResetPasswordModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agamin.Core.Models
+{
+    public class ResetPasswordModel
+    {
+        public string Token { get; set; }
+        public string Password { get; set; }
+    }
+}
